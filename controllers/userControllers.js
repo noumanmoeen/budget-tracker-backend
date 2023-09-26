37df8ae9a14d5144c97a8f83db00f2bd939d77b9
moeennouman7@gmail.com <moeennouman7@gmail.com>
2023-09-26 22:46:08 +0500

    Removed logs

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -23,7 +23,6 @@ export const registerUser = asyncHandler(async (req, res) => {
 
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
-  console.log(hashedPassword , password)
   const user = await User.create({
     name,
     email,
