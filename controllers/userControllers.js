0f50ec43c620f0588bdc01c194f88626d6b1ece4
moeennouman7@gmail.com <moeennouman7@gmail.com>
2023-09-30 23:38:41 +0500

    Sample

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -22,7 +22,7 @@ export const registerUser = asyncHandler(async (req, res) => {
 
   if (!name || !email || !password || !phoneNumber) {
     res.status(400);
-    throw new Error('Please add all fields');
+    throw new Error('Please add all fields ');
   }
 
   const userExists = await User.findOne({ email });
